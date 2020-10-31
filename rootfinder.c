ed0aee60c4da91051df55fc3a4709c80e961143c
Andras Cser <acser@e6410.fios-router.home>
2020-10-31 10:46:43 -0400

    Added GPL and usage comments

--- rootfinder.c
+++ rootfinder.c
@@ -1,3 +1,29 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/* Finds roots of expression in function f() below.
+Usage:
+   rootfinder from to number_of_intervals
+
+Example:
+$ rootfinder -1 2 30
+result: Bisection: -0.53167895748265436850
+result: Bisection: 1.15872847301870973702
+Time spent: 0.00031399999999999999
+
+*/   
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
