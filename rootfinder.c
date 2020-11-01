3cb5d66656f0d9a72da6e366e2c1dc645f90d63d
Andras Cser <acser@e6410.fios-router.home>
2020-11-01 16:13:27 -0500

    Added tinyexpr.c from https://github.com/codeplea/tinyexpr

--- rootfinder.c
+++ rootfinder.c
@@ -12,15 +12,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-/* Finds roots of expression in function f() below.
-Usage:
-   rootfinder from to number_of_intervals
-
-Example:
-$ rootfinder -1 2 30
-result: Bisection: -0.53167895748265436850
-result: Bisection: 1.15872847301870973702
-Time spent: 0.00031399999999999999
+/* Finds roots of expression in  the first argument expression:
+Example
+$ rootfinder "(2*asin((x*x-2)/2)-x*x*(2*asin(x/2)-3.1415)-x*sqrt(4-x*x))/4" -1 2 100
+result: Bisection: -0.53168220620747908001
+result: Bisection: 1.15875664258346633417
+Time spent: 0.00772799999999999963
 
 */   
 
@@ -28,13 +25,34 @@ Time spent: 0.00031399999999999999
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include "tinyexpr.h"
 
 #define EPSILON 0.000000000001 // tolerance, do not decreaase, this is all 'double' in C can do
 #define DX      0.000000000001      // dx, delta x: for numerical differentiation, do not decrease, this is all 'float in C can do
 
+const char *function_arg;
+
 // Function to find the roots for
-double f(double x) {
- return (2*asin((x*x-2)/2)-x*x*(2*asin(x/2)-M_PI)-x*pow(4-x*x, 0.5))/4;
+double f(double x_h) {
+ // Initialize the expression evaluator
+ double x;
+ te_variable vars[] = {{"x", &x}};
+ /* This will compile the expression and check for errors. */
+ int err;
+ te_expr *n = te_compile(function_arg, vars, 1, &err);
+ x=x_h;
+ double r;
+ if (n) {
+  r = te_eval(n);
+  te_free(n);
+ }  else {
+
+   //printf("*debug* Error x=%f; r=%f\n", x, r);
+ }
+ //printf("*debug* x=%f; r=%f\n", x, r);
+ return r;
+
+// return (2*asin((x*x-2)/2)-x*x*(2*asin(x/2)-M_PI)-x*pow(4-x*x, 0.5))/4;
  // return ((x+1)*(x+1)*(x-1)*x*(x+5))*(2*asin((x*x-2)/2)-x*x*(2*asin(x/2)-M_PI)-x*pow(4-x*x, 0.5))/4;
  // return ((x+1)*(x+1)*(x-1)*x*(x+5));
  //return ((x-3)*(x-3)*(x+5));
@@ -118,9 +136,13 @@ int main(int argc, char* argv[]) {
  // Convert first two arguments to double a=lower bound, b=upper bound
  clock_t begin = clock();
 
- double from = strtod(argv[1], NULL); // beginning of the range to look for roots
- double to   = strtod(argv[2], NULL); // end of the range to look for roots
- double num_seg   = strtod(argv[3], NULL); // number of intervals between from and to
+ // Get the expression from the command line
+ function_arg = argv[1];
+
+
+ double from = strtod(argv[2], NULL); // beginning of the range to look for roots
+ double to   = strtod(argv[3], NULL); // end of the range to look for roots
+ double num_seg   = strtod(argv[4], NULL); // number of intervals between from and to
  double result;
  int i=0; // counter
  double h = (to - from)/num_seg; // this is the length of an interval segment
@@ -146,6 +168,7 @@ int flag;
    printf("result: Zero at fih: %.20f\n", result);  
    flag = 0;
   } 
+// Bisection
   if ((flag==1) && (fih*fiph<0)) {  
    result = bisectf (ih, iph);
    printf("result: Bisection: %.20f\n", result);  
